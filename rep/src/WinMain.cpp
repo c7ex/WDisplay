04b8dea810d45e37a0d9f4dda9fbcbea29d6882e
c7ex <c7.exe@mail.ru>
2024-01-01 21:10:57 +0300

    Create window

--- rep/src/WinMain.cpp
+++ rep/src/WinMain.cpp
@@ -7,5 +7,24 @@ int WINAPI WinMain(
 	_In_     LPSTR     lpCmdLine,
 	_In_     int       nCmdShow)
 {
+	// https://learn.microsoft.com/en-us/previous-versions/ms960010(v=msdn.10)
+	HWND hMain = CreateWindowEx(
+		WS_EX_LEFT,
+		WC_DIALOG, 
+		TEXT("Empty window"),
+		WS_VISIBLE | WS_SYSMENU, 
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	// infinite cycle
+	while (1);
+
 	return 0;
 }
+
