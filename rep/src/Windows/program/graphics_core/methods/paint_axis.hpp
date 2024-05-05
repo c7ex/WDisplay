3c64d999009a3841fed36474620ac38431d8a938
c7ex <c7.exe@mail.ru>
2024-05-06 02:05:03 +0300

    clear paint_axis

--- rep/src/Windows/program/graphics_core/methods/paint_axis.hpp
+++ rep/src/Windows/program/graphics_core/methods/paint_axis.hpp
@@ -1,113 +1,104 @@
 #pragma once
-#define START_MAIN_AXIS 0
-#define MIN_SIPPORTIVE_AXIS 10.
-#define MAX_SIPPORTIVE_AXIS 20.
 
-// calculation optimal discretization
-double get_discretization_supportive_axis(double abstract_limit)
+// abstract coordination
+typedef double _Acrd;
+// mouse coordination
+typedef double _Mcrd;
+
+#define ZEROVALUE 0
+#define BASELOG10 10
+
+#define SIPPORTIVE_AXIS_MIN 10.
+#define SIPPORTIVE_AXIS_MAX 25.
+#define SIPPORTIVE_AXIS_BACKLASH_START 15
+#define SIPPORTIVE_AXIS_BACKLASH_END 15
+
+// calculation optimal discretization for supportive axis
+double get_discretization_supportive_axis(_Acrd limit)
 {
-	double coarse_discretization = abstract_limit / MIN_SIPPORTIVE_AXIS;
-	double order = round(log10(coarse_discretization));
-	double discretization = pow(10, order);
-	double number_of_axes = abstract_limit / discretization;
+	// max abstract discretization
+	double coarse_discretization = limit / SIPPORTIVE_AXIS_MIN;
+	double order_scale = round(log10(coarse_discretization));
+	double discretization = pow(BASELOG10, order_scale);
+	double number_of_axes = limit / discretization;
 
-	while ((number_of_axes > MAX_SIPPORTIVE_AXIS) || (number_of_axes < MIN_SIPPORTIVE_AXIS))
+	// search optimal discretization
+	while ((number_of_axes > SIPPORTIVE_AXIS_MAX) || (number_of_axes < SIPPORTIVE_AXIS_MIN))
 	{
-		if (number_of_axes >= MAX_SIPPORTIVE_AXIS)
-			discretization *= MIN_SIPPORTIVE_AXIS;
-		else if (number_of_axes <= MIN_SIPPORTIVE_AXIS)
-			discretization /= (MAX_SIPPORTIVE_AXIS/ MIN_SIPPORTIVE_AXIS);
-		number_of_axes = (abstract_limit / discretization);
+		if (number_of_axes >= SIPPORTIVE_AXIS_MAX)
+			discretization *= SIPPORTIVE_AXIS_MIN;
+		else if (number_of_axes <= SIPPORTIVE_AXIS_MIN)
+			discretization /= (SIPPORTIVE_AXIS_MAX / SIPPORTIVE_AXIS_MIN);
+		number_of_axes = (limit / discretization);
 	}
 
 	return discretization;
 }
 
-void paint_once_supportive_axis_label_x(HDC& hMemDc, double a_pos_x, double a_pos_y, double var)
+void paint_once_supportive_axis_label(HDC& hMemDc, _Acrd x, _Acrd y, double value)
 {
-	double m_pos_x = get_mouse_coordinate_x(a_pos_x);
-	double m_pos_y = get_mouse_coordinate_y(a_pos_y);
+	if (value == ZEROVALUE) return;
 
-	if (m_pos_y < 5)
-		m_pos_y = 5;
-	else if (m_pos_y > gc.window.count_pixels.y - 15)
-		m_pos_y = gc.window.count_pixels.y - 15;
+	_Mcrd coordinate_x = get_mouse_coordinate_x(x);
 
-	paint_text(hMemDc, m_pos_x, m_pos_y, var);
+	if (coordinate_x < SIPPORTIVE_AXIS_BACKLASH_START)
+		coordinate_x = SIPPORTIVE_AXIS_BACKLASH_START;
+	else if (coordinate_x > gc.window.count_pixels.x - SIPPORTIVE_AXIS_BACKLASH_END)
+		coordinate_x = gc.window.count_pixels.x - SIPPORTIVE_AXIS_BACKLASH_END;
+
+	_Mcrd coordinate_y = get_mouse_coordinate_y(y);
+
+	if (coordinate_y < SIPPORTIVE_AXIS_BACKLASH_START)
+		coordinate_y = SIPPORTIVE_AXIS_BACKLASH_START;
+	else if (coordinate_y > gc.window.count_pixels.y - SIPPORTIVE_AXIS_BACKLASH_END)
+		coordinate_y = gc.window.count_pixels.y - SIPPORTIVE_AXIS_BACKLASH_END;
+
+	paint_text(hMemDc, coordinate_x, coordinate_y, value);
 }
 
-void paint_once_supportive_axis_label_y(HDC& hMemDc, double a_pos_x, double a_pos_y, double var)
+void paint_once_horizontal_axis(HDC& hMemDc, _Acrd y)
 {
-	if (var == 0) return;
-
-	double m_pos_x = get_mouse_coordinate_x(a_pos_x);
-	double m_pos_y = get_mouse_coordinate_y(a_pos_y);
-
-	if (m_pos_x < 5)
-		m_pos_x = 5;
-	else if (m_pos_x > gc.window.count_pixels.x - 15)
-		m_pos_x = gc.window.count_pixels.x - 15;
-
-	paint_text(hMemDc, m_pos_x, m_pos_y, var);
+	_Mcrd coordinate_y = get_mouse_coordinate_y(y);
+	if (coordinate_y > ZEROVALUE)
+		paint_line(hMemDc, ZEROVALUE, coordinate_y, gc.window.count_pixels.x, coordinate_y);
 }
 
-void hMainAxis(HDC& hMemDc, double y)
+void paint_once_vertical_axis(HDC& hMemDc, _Acrd x)
 {
-	double my = get_mouse_coordinate_y(y);
-	if (my > 0)
-		paint_line(hMemDc, 0, my, gc.window.count_pixels.x, my);
-}
-
-void vMainAxis(HDC& hMemDc, double x)
-{
-	double mx = get_mouse_coordinate_x(x);
-	if (mx >= 0)
-		paint_line(hMemDc, mx, 0, mx, gc.window.count_pixels.y);
-}
-
-void horizontal_supportive_axis(HDC& hMemDc, double y)
-{
-	double my = get_mouse_coordinate_y(y);
-	if (my > 0)
-		paint_line(hMemDc, 0, my, gc.window.count_pixels.x, my);
-}
-
-void vertical_supportive_axis(HDC& hMemDc, double x)
-{
-	double mx = get_mouse_coordinate_x(x);
-	if (mx >= 0)
-		paint_line(hMemDc, mx, 0, mx, gc.window.count_pixels.y);
+	_Mcrd coordinate_x = get_mouse_coordinate_x(x);
+	if (coordinate_x >= ZEROVALUE)
+		paint_line(hMemDc, coordinate_x, NULL, coordinate_x, gc.window.count_pixels.y);
 }
 
 void paint_main_axis(HDC& hMemDc)
 {
 	SelectObject(hMemDc, reinterpret_cast<HGDIOBJ>(stock_objects::pen.main_axis));
-	hMainAxis(hMemDc, START_MAIN_AXIS);
-	vMainAxis(hMemDc, START_MAIN_AXIS);
+	paint_once_horizontal_axis(hMemDc, ZEROVALUE);
+	paint_once_vertical_axis(hMemDc, ZEROVALUE);
 }
 
 void paint_supportive_axis(HDC& hMemDc)
 {
-	double d_x = get_discretization_supportive_axis(gc.window.coordinates_limit.x);
-	double d_y = get_discretization_supportive_axis(gc.window.coordinates_limit.y);
+	_Acrd discret_x = get_discretization_supportive_axis(gc.window.coordinates_limit.x);
+	_Acrd discret_y = get_discretization_supportive_axis(gc.window.coordinates_limit.y);
 
-	double r_x = round(gc.window.coordinates_begin.x / d_x);
-	double r_y = round(gc.window.coordinates_end.y / d_y);
+	int round_count_x = round(gc.window.coordinates_begin.x / discret_x);
+	int round_count_y = round(gc.window.coordinates_end.y / discret_y);
 
-	if ((r_x * d_x) < gc.window.coordinates_begin.x) r_x++;
+	if ((round_count_x * discret_x) < gc.window.coordinates_begin.x) round_count_x++;
 
-	while (r_x * d_x <= gc.window.coordinates_end.x)
+	while (round_count_x * discret_x <= gc.window.coordinates_end.x)
 	{
-		vertical_supportive_axis(hMemDc, r_x * d_x);
-		r_x++;
+		paint_once_vertical_axis(hMemDc, round_count_x * discret_x);
+		round_count_x++;
 	}
 
-	if ((r_y * d_y) < gc.window.coordinates_end.y) r_y++;
+	if ((round_count_y * discret_y) < gc.window.coordinates_end.y) round_count_y++;
 
-	while (r_y * d_y <= gc.window.coordinates_begin.y)
+	while (round_count_y * discret_y <= gc.window.coordinates_begin.y)
 	{
-		horizontal_supportive_axis(hMemDc, r_y * d_y);
-		r_y++;
+		paint_once_horizontal_axis(hMemDc, round_count_y * discret_y);
+		round_count_y++;
 	}
 }
 
@@ -115,21 +106,21 @@ void paint_supportive_axis_labels(HDC& hMemDc)
 {
 	SetTextColor(hMemDc, stock_objects::color.white);
 
-	double d_x = get_discretization_supportive_axis(gc.window.coordinates_limit.x);
-	double d_y = get_discretization_supportive_axis(gc.window.coordinates_limit.y);
+	_Acrd discret_x = get_discretization_supportive_axis(gc.window.coordinates_limit.x);
+	_Acrd discret_y = get_discretization_supportive_axis(gc.window.coordinates_limit.y);
 
-	double r_x = round(gc.window.coordinates_begin.x / d_x);
-	double r_y = round(gc.window.coordinates_end.y / d_y);
+	int round_count_x = round(gc.window.coordinates_begin.x / discret_x);
+	int round_count_y = round(gc.window.coordinates_end.y / discret_y);
 
-	while (r_x * d_x <= gc.window.coordinates_end.x)
+	while (round_count_x * discret_x <= gc.window.coordinates_end.x)
 	{
-		paint_once_supportive_axis_label_x(hMemDc, r_x * d_x, 0, r_x * d_x);
-		r_x++;
+		paint_once_supportive_axis_label(hMemDc, round_count_x * discret_x, ZEROVALUE, round_count_x * discret_x);
+		round_count_x++;
 	}
 
-	while (r_y * d_y <= gc.window.coordinates_begin.y)
+	while (round_count_y * discret_y <= gc.window.coordinates_begin.y)
 	{
-		paint_once_supportive_axis_label_y(hMemDc, 0, r_y * d_y, r_y * d_y);
-		r_y++;
+		paint_once_supportive_axis_label(hMemDc, ZEROVALUE, round_count_y * discret_y, round_count_y * discret_y);
+		round_count_y++;
 	}
 }
